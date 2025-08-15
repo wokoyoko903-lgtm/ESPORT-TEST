b83bc38e0054527b42ae94ef081894fa56709536
wokoyoko903-lgtm <wokoyoko903@gmail.com>
2025-08-15 12:30:46 +0200

    Add files via upload

---
+++ esport_manager_game/script.js
@@ -0,0 +1,6 @@
+let week = 1;
+function nextWeek() {
+    week++;
+    document.getElementById('status').innerText = "Semaine actuelle : " + week;
+}
+document.getElementById('status').innerText = "Semaine actuelle : " + week;
